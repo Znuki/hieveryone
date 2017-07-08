3f402b168b13175ae4541d092ab02eed5855388a
Jinwook Kang <wlsdnrdl99@naver.com>
2017-07-08 08:43:46 +0000

    Read 했어요

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,5 +7,18 @@ class PostsController < ApplicationController
         @new_post.title = params[:input_title] #2. title이란 곳에 params로 제목을 저장한다.
         @new_post.content = params[:input_content]#3. content란 곳에 params로 내용을 저장한다.
         @new_post.save #4. 완전히 저장한다.
+        
+        # 저장했으니까 여기로 가렴.
+        redirect_to "/posts/show/#{@new_post.id}"
+    end
+    def index # 모든 게시글을 보여주는 것
+        @posts = Post.all
+    
+    end
+    
+    
+    def show # 하나의 게시글을 보여주는 것
+        @post = Post.find(params[:id])
+
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,14 @@ Rails.application.routes.draw do
    root "posts#new"# 맨처음 홈 지정을 root로 해준다.
   get 'home/index'
   get 'home/result' => 'home#result' # 홈컨트롤러로 결과를 보내준다. 응답을 result에서 해준다.
+  
+  #CRUD 중 Create에 관련된 내용입니다.
   get 'posts/new'
   post 'posts/create' => 'posts#create'
+  
+  #CRUD 중 Read에 관뢴된 내용입니다.
+  get 'posts/index'
+  get 'posts/show/:id' => 'posts#show'
   
   
   # The priority is based upon order of creation: first created -> highest priority.
